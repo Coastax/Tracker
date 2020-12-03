038f614e7deaf83da82667ea5e85c2ad2b4d231f
Xiaoyi Zhao <xiaoyiz@sfu.ca>
2020-12-03 03:32:04 -0800

    Added function to start recording path, makers to indicate start and end points

--- app/src/main/java/com/example/tracker/ui/main/MapsFragment.java
+++ app/src/main/java/com/example/tracker/ui/main/MapsFragment.java
@@ -54,7 +54,6 @@ public class MapsFragment extends Fragment {
     LocationRequest mLocationRequest;
     Location mLastLocation;
     Location mNewLocation;
-    Marker mCurrLocationMarker;
     FusedLocationProviderClient mFusedLocationClient;
     TravelPath currentPath;
     FloatingActionButton startBtn;
@@ -116,6 +115,7 @@ public class MapsFragment extends Fragment {
                 if (recording) {
                     if (currentPath.getSize() == 0) {
                         currentPath.addLocation(location);
+                        redrawPolyLine();
                     }
                     else if (delta >= 1) {
                         currentPath.addLocation(location);
@@ -147,7 +147,7 @@ public class MapsFragment extends Fragment {
         startMaker.position(new LatLng(startPoint.getLatitude(), startPoint.getLongitude()));
         startMaker.title("Start");
         startMaker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-        mCurrLocationMarker = mGoogleMap.addMarker(startMaker);
+        mGoogleMap.addMarker(startMaker);
     }
 
     @Nullable
@@ -185,7 +185,7 @@ public class MapsFragment extends Fragment {
                 endMaker.position(new LatLng(endPoint.getLatitude(), endPoint.getLongitude()));
                 endMaker.title("End");
                 endMaker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-                mCurrLocationMarker = mGoogleMap.addMarker(endMaker);
+                mGoogleMap.addMarker(endMaker);
 
 
                 recording = false;
